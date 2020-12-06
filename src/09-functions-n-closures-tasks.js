86f5bae57f155ce3e28ac04137e7d54392a5884c
burik84 <avburik84@gmail.com>
2020-12-06 11:55:15 +0300

    feat: start solution 09

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (x) => f(g(x));
 }
 
 
@@ -44,8 +44,10 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  const power2 = (x) => x ** exponent;
+  const power05 = (x) => x ** exponent;
+  return (z) => (exponent === 2 ? power2(z) : power05(z));
 }
 
 
